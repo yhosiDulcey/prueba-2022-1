d221af86061206f4f82d66e955706850b6e46b61
Yhosi Dulcey <yhesteban17@gmail.com>
2022-09-06 21:42:59 -0500

    creacion de pages estadios

---
+++ Torneo.App.Frontend/Pages/Estadios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Torneo.App.Frontend.Pages.Estadios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
