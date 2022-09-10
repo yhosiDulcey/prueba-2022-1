fa412b28160b0e1d98ec8c604b00a2050c7d795d
paiitodev <yuyis1999@hotmail.com>
2022-09-10 08:45:25 -0500

    Create Equipos

--- Torneo.App.Dominio/Entidades/Municipio.cs
+++ Torneo.App.Dominio/Entidades/Municipio.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Torneo.App.Dominio
 {
     public class Municipio
     {
         public int Id { get; set; }
+       
+        [Display(Name = "Nombre del municipio")]
         public string Nombre { get; set; }
     }
 }
---
+++ Torneo.App.Frontend/Pages/DTs/Create.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+
+namespace Torneo.App.Frontend.Pages.DTs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioDT _repoDT;
+        public CreateModel(IRepositorioDT repoDT)
+        {
+            _repoDT = repoDT;
+        }
+        public DirectorTecnico dt {get; set;}
+        public void OnGet()
+        {
+            dt = new DirectorTecnico();
+
+        }
+        public IActionResult OnPost(DirectorTecnico dt)
+        {
+            _repoDT.AddDT(dt);
+            return RedirectToPage("Index");
+        }
+    }
+}
