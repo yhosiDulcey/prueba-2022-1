d19ece7001f2a85a5d3cf87f937c8c7dcd127ebf
david700427 <david700427@gmail.com>
2022-09-11 12:31:45 -0500

    Creacion index Jugadores 2

--- Torneo.App.Frontend/Program.cs
+++ Torneo.App.Frontend/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IRepositorioMunicipio, RepositorioMunicipio>();
 builder.Services.AddSingleton<IRepositorioDT, RepositorioDT>();
 builder.Services.AddSingleton<IRepositorioEquipo, RepositorioEquipo>();
+builder.Services.AddSingleton<IRepositorioJugador, RepositorioJugador>();
+
 
 var app = builder.Build();
 
