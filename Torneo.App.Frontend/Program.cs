d19acaa4b834fdfd778e8e604cd1374212099dab
paiitodev <yuyis1999@hotmail.com>
2022-09-13 17:33:55 -0500

    Index y Details Estadio

--- Torneo.App.Frontend/Program.cs
+++ Torneo.App.Frontend/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IRepositorioMunicipio, RepositorioMunicipio>();
 builder.Services.AddSingleton<IRepositorioDT, RepositorioDT>();
 builder.Services.AddSingleton<IRepositorioEquipo, RepositorioEquipo>();
+builder.Services.AddSingleton<IRepositorioEstadio, RepositorioEstadio>();
 
 var app = builder.Build();
 
